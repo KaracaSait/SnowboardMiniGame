07a636379fb233e887ccfa8a37257918563500fb
Sait Karaca <sait.karaca1@gmail.com>
2024-02-07 01:40:31 +0300

    first commit

---
+++ Assets/Script/Crash.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crash : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Ground")
+        {
+            Debug.Log("Fail");
+        }
+    }
+}
