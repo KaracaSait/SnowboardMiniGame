196918b39f59b1cfa812724d02d369c85a77e1e9
Sait Karaca <sait.karaca1@gmail.com>
2024-02-07 02:16:34 +0300

    Crash and Finish Line Added

--- Assets/Script/Crash.cs
+++ Assets/Script/Crash.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Crash : MonoBehaviour
 {
+    [SerializeField] float crashTime = 0.5f;
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
-            Debug.Log("Fail");
+            Invoke("ReloadScene", crashTime);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
