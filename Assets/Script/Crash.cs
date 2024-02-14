c1b3cb6f60e7f48dd93f7397bc567acb8b1ae23d
Sait Karaca <sait.karaca1@gmail.com>
2024-02-14 08:30:12 +0300

    project completed

--- Assets/Script/Crash.cs
+++ Assets/Script/Crash.cs
@@ -7,11 +7,16 @@ public class Crash : MonoBehaviour
 {
     [SerializeField] float crashTime = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+    bool hasCrash = false;
     void OnTriggerEnter2D(Collider2D other) 
     {
-        if(other.tag == "Ground")
+        if(other.tag == "Ground" && hasCrash == false)
         {
+            hasCrash = true;
+            FindObjectOfType<PlayerController>().DisableMove();
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", crashTime);
         }
     }
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -10,6 +10,8 @@ public class PlayerController : MonoBehaviour
   [SerializeField] float normalSpeed = 20f;
   Rigidbody2D rb2d;
   SurfaceEffector2D surfaceEffector2D;
+
+  bool canMove = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +22,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      SpeedPlayer();
-      RotatePlayer();
+      if (canMove == true)
+      {
+        SpeedPlayer();
+        RotatePlayer();
+      }
     }
 
+    public void DisableMove()
+    {
+      canMove = false;
+    }
 
     void SpeedPlayer()
     {
